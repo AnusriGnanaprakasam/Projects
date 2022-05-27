b91110d0e4b98f94dda62b33cb3d5059a20aaacc
Anusri Dillibabu Gnanaprakasam <87781785+AnusriGnanaprakasam@users.noreply.github.com>
2022-05-27 08:46:58 -0700

    how an old setup.py file would look like

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
-from setuptools import setup, find_packages# had to edit python .json settings file
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 setup(
-    name = 'Projects',
+    name = 'Life_Manager',
     version = '0.0.1',
     author = 'ADG',
     author_email = 'anusri.gnanaprakasom@gmail.com',
-    #license = '<the license you chose>',
     description = 'organize your stuff',
     long_description = long_description,
     long_description_content_type = "text/markdown",
     url = 'https://github.com/AnusriGnanaprakasam/Projects',
     include_package_data=True,
-    py_modules = ['projects,cli'],
-    packages = ["projects"],
+    py_modules = ['LifeManager'],
+    packages = ["LifeManager"],
     install_requires = [requirements],
     python_requires='>=3.7',
     classifiers=[
         "Programming Language :: Python :: 3.9",
         "Operating System :: Windows ",
     ],
-    entry_points = {'console_scripts':['life-manager=projects:main']}#figure out how to do arguments for this later
+    entry_points = {'console_scripts':['life-manager=LifeManager.main:main']}
 )
